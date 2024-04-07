09f145e657107d8bb15a000923e02eb7d22d38d6
Chuba-Nwene. C <chinemelumchubanwene57@gmail.com>
2024-04-07 12:47:12 +0200

    add Nav component and build layout and page components for home route

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import Feed from "@components/Feed"
+
+const Home = () => {
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden"/> <span className="orange_gradient">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Prompt Hub is an open-sourced AI propmting hub for users to discover, create and share creative prompts
+      </p>
+
+      <Feed />
+    </section>
+  )
+}
+
+export default Home   
